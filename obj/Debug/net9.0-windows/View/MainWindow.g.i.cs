b377a49a4da8c107936fa4a356e7121430644e58
Bautz Nikolas | BMZ Germany <nikolas.bautz@bmz-group.com>
2025-03-21 09:05:05 +0100

    final

--- obj/Debug/net9.0-windows/View/MainWindow.g.i.cs
+++ obj/Debug/net9.0-windows/View/MainWindow.g.i.cs
@@ -101,7 +101,7 @@ namespace Test.View {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/Test;V1.0.0.0;component/view/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/Test;component/view/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\..\View\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
